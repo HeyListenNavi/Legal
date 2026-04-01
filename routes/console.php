70cb36b3e46da10374a90da94fecf39d0bad4cbb
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-04-01 09:45:47 -0700

    add: payment notification schedules

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Console\Commands\NotifyDuePayments;
+use App\Console\Commands\NotifyEarlyPayments;
+use App\Console\Commands\NotifyLatePayments;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command(NotifyEarlyPayments::class)->everyMinute();
+Schedule::command(NotifyDuePayments::class)->everyMinute();
+Schedule::command(NotifyLatePayments::class)->everyMinute();
