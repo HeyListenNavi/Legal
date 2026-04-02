eb891bfe4278fed647868956b2a84c63c31cd4c3
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-04-02 00:34:49 -0700

    fix: make has whatsapp column in the user table a boolean

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string("phone_number")->nullable();
-            $table->string("has_wa")->nullable();
+            $table->boolean("has_wa")->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
